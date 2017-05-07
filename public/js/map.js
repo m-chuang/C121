c5a73969e383c68517799d4a43f7a20ddae10559
Michael C <Michael C>
2017-05-07 14:46:02 -0700

    Cleaned up map.js; disabled error range

--- public/js/map.js
+++ public/js/map.js
@@ -9,36 +9,9 @@ var errorCircle;
 var center;
 var zoomLevel;
 
-//////////////////// CENTER AT LOCATION CONTROLS ////////////////////
-function CenterControl(controlDiv, map) {
-  // Set CSS for the control border.
-  var controlUI = document.createElement('div');
-  controlUI.style.backgroundColor = '#fff';
-  controlUI.style.border = '2px solid #fff';
-  controlUI.style.borderRadius = '3px';
-  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.marginBottom = '22px';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to recenter the map';
-  controlDiv.appendChild(controlUI);
+//////////////////// CONTINUOUSLY UPDATE LOCATION ////////////////////
+autoUpdate();
 
-  // Set CSS for the control interior.
-  var controlText = document.createElement('div');
-  controlText.style.color = 'rgb(25,25,25)';
-  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-  controlText.style.fontSize = '16px';
-  controlText.style.lineHeight = '38px';
-  controlText.style.paddingLeft = '5px';
-  controlText.style.paddingRight = '5px';
-  controlText.innerHTML = 'Center at Current Location';
-  controlUI.appendChild(controlText);
-
-  // Setup the click event listeners.
-  controlUI.addEventListener('click', function() {
-    map.setCenter(center);
-  });
-}
 
 
 //////////////////// MAP INITIALIZATION ////////////////////
@@ -60,7 +33,6 @@ function initMap() {
 
   // HEATMAP INITIALIZATION
   heatmap = new google.maps.visualization.HeatmapLayer({
-    //data: getPoints(),
     data: generateRandomPoints(), // ARRAY OF POINTS TO LOAD
     map: map
   });
@@ -108,8 +80,6 @@ function initMap() {
 
 
 
-
-
       ////////////////////// WIP /////////////////////
 
 
@@ -124,40 +94,137 @@ function initMap() {
 
       google.maps.event.addDomListener(window, 'load', initialize);
 
-      ////////////////////// WIP /////////////////////
+      ////////////////////// WIP /////////////////////    
 
-    
-  }, function() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+  	}, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   } else {
-    map.setCenter(defaultCenter);
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
+  	  // Center at default location
+      map.setCenter(defaultCenter);
+      handleLocationError(false, infoWindow, map.getCenter());
   }
 
 
-
+  // CREATE CENTER AT LOCATION BUTTON
   var centerControlDiv = document.createElement('div');
   var centerControl = new CenterControl(centerControlDiv, map);
 
   centerControlDiv.index = 1;
   map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(centerControlDiv);
 
+
+  // INITIALIZE FIREBASE
   initFirebase();
 }
 
 
 
+//////////////////// LOCATION UPDATER ////////////////////
+function autoUpdate() {
+  navigator.geolocation.getCurrentPosition(function(position) {  
+    var newPoint = new google.maps.LatLng(position.coords.latitude, 
+                                          position.coords.longitude);
+
+    // LOCATION MARKER UPDATER
+    if (marker) {
+      marker.setPosition(newPoint);
+    }
+    else {
+      marker = new google.maps.Marker({
+        position: newPoint,
+        icon: icon,
+        map: map
+      });
+    }
+
+
+    // ERROR RANGE UPDATER
+    /*
+    if(errorCircle){
+      errorCircle.setMap(null);
+    }
+    else {
+      errorCircle = new google.maps.Circle({
+        strokeColor: '#1976D2',
+        strokeOpacity: 0.8,
+        strokeWeight: 3,
+        fillColor: '#1976D2',
+        fillOpacity: 0.35,
+        map: map,
+        center: newPoint,
+        radius: 100
+    })
+    }
+    */
+
+  });
+
+  // UPDATE EVERY SECOND
+  setTimeout(autoUpdate, 1000);
+}
 
 
 
+//////////////////// LOCATION ERROR HANDLING ////////////////////
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: Ferret cannot find your location.' :
+    'Error: Your browser doesn\'t support geolocation.');
+     infoWindow.open(map);
+}
 
 
 
+//////////////////// CENTER AT LOCATION CONTROLS ////////////////////
+function CenterControl(controlDiv, map) {
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginBottom = '22px';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to recenter the map';
+  controlDiv.appendChild(controlUI);
 
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '16px';
+  controlText.style.lineHeight = '38px';
+  controlText.style.paddingLeft = '5px';
+  controlText.style.paddingRight = '5px';
+  controlText.innerHTML = 'Center at Current Location';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners.
+  controlUI.addEventListener('click', function() {
+    map.setCenter(center);
+  });
+}
+
+
+
+//////////////////// FIREBASE INITIALIZATION ////////////////////
 function initFirebase() {
-
   var config = {
     apiKey: "AIzaSyD3R60ccA0wuNN40tV_aLPGQZl4yrUjcTQ",
     authDomain: "ferret-82825.firebaseapp.com",
@@ -166,6 +233,7 @@ function initFirebase() {
     storageBucket: "ferret-82825.appspot.com",
     messagingSenderId: "285929520709"
   };
+  
   firebase.initializeApp(config);
 
    // FIREBASE
@@ -183,88 +251,12 @@ function initFirebase() {
           console.log('not logged in');
           window.location = './';
         }
-
     });
-
-}
-// Error message for if can't gather current location
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-    'Error: Ferret cannot find your location.' :
-    'Error: Your browser doesn\'t support geolocation.');
-     infoWindow.open(map);
 }
 
 
-// autoUpdate to track user location data
-function autoUpdate() {
-  navigator.geolocation.getCurrentPosition(function(position) {  
-    var newPoint = new google.maps.LatLng(position.coords.latitude, 
-                                          position.coords.longitude);
 
-    if(errorCircle != null){
-      errorCircle.setMap(null);
-    }
-
-    if (marker) {
-      // Marker already created - move to current location
-      marker.setPosition(newPoint);
-    }
-    else {
-      marker = new google.maps.Marker({
-        position: newPoint,
-        icon: icon,
-        map: map
-      });
-    }
-
-    zoomLevel = map.getZoom();
-    radiusVal = zoomLevel*6;
-
-    errorCircle = new google.maps.Circle({
-      strokeColor: '#1976D2',
-      strokeOpacity: 0.8,
-      strokeWeight: 3,
-      fillColor: '#1976D2',
-      fillOpacity: 0.35,
-      map: map,
-      center: newPoint,
-      // TODO: zoom*val = radius for zoom in/out
-      radius: radiusVal
-    })
-
-
-    // Centering map in new position
-    //map.setCenter(newPoint);
-  });
-
-  // Calling autoUpdate every second
-  setTimeout(autoUpdate, 1000);
-}
-autoUpdate();
-
-
-// Get sample map data points (used for the Heatmap)
-function getPoints() {
-  var pointsArr = [
-    new google.maps.LatLng(32.8796116,-117.2358329),
-    new google.maps.LatLng(32.8818006,-117.2335235),
-    new google.maps.LatLng(32.8818006,-117.2335235),
-    new google.maps.LatLng(32.8917558,-117.241156),
-    new google.maps.LatLng(32.878178205413782,-117.237203613930447),
-    new google.maps.LatLng(32.8747486,-117.242025799999979),
-    new google.maps.LatLng(32.8747486,-117.242025799999979),
-    new google.maps.LatLng(32.879713,-117.2365667),
-    new google.maps.LatLng(32.8917558,-117.241156),
-    new google.maps.LatLng(32.8841462,-117.2428555),
-    new google.maps.LatLng(32.8832821,-117.2387677),
-  ];
-  console.log(pointsArr);
-  return pointsArr;
-}
-
-// generate random points as sample data
+//////////////////// LOCATION POINTS GENERATOR ////////////////////
 function generateRandomPoints(){
 	var randomPoints = [];
 	for( var i = 0; i < 1000; i++){
@@ -279,13 +271,16 @@ function generateRandomPoints(){
 	return randomPoints;
 }
 
-// helper for generating a random number
+
 function randomIntFromInterval(min,max)
 {
    return Math.random()/100;
 }
 
-// change the gradient of the heatmap to a "reverse" heatmap
+
+
+//////////////////// HEATMAP FUNCTIONS ////////////////////
+// CHANGE GRADIENT TO 'REVERSE' HEATMAP
 function changeGradient() {
   var gradient = [
     'rgba(49, 197, 57, 1)', // default green overlay
@@ -306,12 +301,14 @@ function changeGradient() {
   heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
 }
 
-// change the opacity of the whole heatmap
+
+// CHANGE OPACITY OF HEATMAP
 function changeOpacity() {
   heatmap.set('opacity', heatmap.get('opacity') ? null : 0.55);
 }
 
-// change the radius of a heatmap point
+
+// CHANGE RADIUS OF HEATMAP POINTS
 function changeRadius() {
   heatmap.set('radius', heatmap.get('radius') ? null : 15);
 }
